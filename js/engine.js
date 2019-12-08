752edaee5d43ed8c240e2eda5374b8c68252ce40
arbaieffendi <arbaieffendi@gmail.com>
2019-12-08 21:09:10 +0700

    add logic for timer

--- js/engine.js
+++ js/engine.js
@@ -1,43 +1,67 @@
 document.getElementById("play_button").onclick = function() {
-    alert("play_button clicked");
+    try{
+        startTimer();
+    } catch(err){
+        alert("500 Sorry, seems there is a problem internally &#x1F927;");
+    }
 }
 
 document.getElementById("add_button").onclick = function() {
-    alert("add_button clicked");
+    try{
+        throw "Not Implemented yet";
+    } catch(err){
+        alert("500 Sorry, seems there is a problem internally &#x1F927;");
+    }
 }
 
 //#region Timer
-function playTimer(){
+function startTimer(){
+    var interval = document.getElementById("countDownTimer");
+    var x = setInterval( function () {
+    
+        // Find the distance between now and the count down date
+        var distance = countDownDate - interval;
+    
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        
+        document.getElementById("timer").innerHTML = minutes + "m " + seconds + "s ";
 
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("timer").innerHTML = "Finish &#x1F947; congrats !";
+        }
+        
+    }, 1000);
 }
 
 function stopTimer(){
-
+    throw "Not implemented yet";
 }
 //#endregion
 
 //#region ToDo
 function addToDo(){
-
+    throw "Not implemented yet";
 }
 
 function checkToDo(){
-
+    throw "Not implemented yet";
 }
 
 function uncheckToDo(){
-
+    throw "Not implemented yet";
 }
 
 function deleteToDo(){
-
+    throw "Not implemented yet";
 }
 
 function clearToDo(){
-
+    throw "Not implemented yet";
 }
 
 function exportToDo(){
-    
+    throw "Not implemented yet";
 }
 //#endregion
