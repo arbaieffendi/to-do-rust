0b248947a404a23ad4b3a6299b2af587df2ded00
arbaieffendi <arbaieffendi@gmail.com>
2019-12-08 08:48:01 +0700

    first mockup

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# to-do-rust
-To Do Rust is a to-do app with enhanced timer feature. It help me plan my day. You can try it here arbaieffendi.github.io/to-do-rust
+# To-do-rust
+To Do Rust is a web-based to-do with enhanced timer feature. It help me plan my day. You can try it here arbaieffendi.github.io/to-do-rust
+
+# Credit
+- Bootstrap 4.4
+- Font Awesome
+
+Have a nice day.
+
+Visit [arba.dev](http://arba.dev)
---
+++ js/engine.js
@@ -0,0 +1,43 @@
+document.getElementById("play_button").onclick = function() {
+    alert("play_button clicked");
+}
+
+document.getElementById("add_button").onclick = function() {
+    alert("add_button clicked");
+}
+
+//#region Timer
+function playTimer(){
+
+}
+
+function stopTimer(){
+
+}
+//#endregion
+
+//#region ToDo
+function addToDo(){
+
+}
+
+function checkToDo(){
+
+}
+
+function uncheckToDo(){
+
+}
+
+function deleteToDo(){
+
+}
+
+function clearToDo(){
+
+}
+
+function exportToDo(){
+    
+}
+//#endregion
