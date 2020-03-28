a8950e54c4615585b580073a4f0c883a5b8db0ab
Arba'i Effendi <arba.jtk@gmail.com>
2020-03-28 19:22:55 +0700

    add id to todo item class for managing purpose

--- js/init.js
+++ js/init.js
@@ -4,23 +4,25 @@ function loadToDoList(){
 
     let todoItems = JSON.parse(localStorage.getItem('todo'));
 
-    // clear the list
-    document.getElementById("todolist").innerHTML = "";
-
-    // update the display todo list
-    todoItems.forEach((item) => {
-        console.log("let me see the item");
-        console.log(item);
+    if (todoItems !== null) {
+        // clear the list
+        document.getElementById("todolist").innerHTML = "";
         
-        document.getElementById("todolist").innerHTML += `
-        <li draggable="true" class="list-group-item d-flex justify-content-between lh-condensed">
-            <div>
-                <h6 class="my-0">`+ item.title +`</h6>
-                <small class="text-muted">`+ item.description +`</small>
-            </div>
-            <span class="text-muted">12 min</span>
-        </li>
-        `;
-    });
+        // update the display todo list
+        todoItems.forEach((item) => {
+            console.log("let me see the item");
+            console.log(item);
+            
+            document.getElementById("todolist").innerHTML += `
+            <li draggable="true" class="list-group-item d-flex justify-content-between lh-condensed">
+                <div>
+                    <h6 class="my-0">`+ item.title +`</h6>
+                    <small class="text-muted">`+ item.description +`</small>
+                </div>
+                <span class="text-muted">12 min</span>
+            </li>
+            `;
+        });
+    }
 
 }
